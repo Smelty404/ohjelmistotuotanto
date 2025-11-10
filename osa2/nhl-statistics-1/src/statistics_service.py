84fba2c2b1d7e8d3d3155d914684c5199fe52da2
Smelty404 <milo.hankama@hotmail.com>
2025-11-10 21:27:08 +0200

    osa2 tehtävät

--- osa2/nhl-statistics-1/src/statistics_service.py
+++ osa2/nhl-statistics-1/src/statistics_service.py
@@ -1,10 +1,8 @@
-from player_reader import PlayerReader
-
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, players):
+        reader = players
 
         self._players = reader.get_players()
 
